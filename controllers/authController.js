a104cc1b9caf0efb987055209aea14d493ef8ad9
Arshman Afzal <arshman3@gmail.com>
2022-09-26 23:59:00 +0500

    updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,18 +9,16 @@ exports.postAuth = (req, res, next) => {
     // console.log(req.body.first_name);
     // res.send(req.body.first_name);
 
-    var first_name = req.body.first_name;
-    var last_name = req.body.last_name;
+    var name = req.body.name;
     var email_address = req.body.email_address;
     var password = req.body.password;
     var gender = req.body.gender;
-    var age = req.body.age;
     var fcmToken = req.body.fcmToken;
 
     const salt = genSaltSync(10);
     password = hashSync(req.body.password, salt);
 
-    db.execute('INSERT INTO users(first_name, last_name, email_address, password, gender, age, fcmToken) VALUES (?, ?, ?, ?, ?, ?, ?)', [first_name, last_name, email_address, password, gender, age, fcmToken]).then(([rows, fieldData]) => {
+    db.execute('INSERT INTO users(name, email_address, password, gender, fcmToken) VALUES (?, ?, ?, ?, ?)', [name, email_address, password, gender, fcmToken]).then(([rows, fieldData]) => {
         res.status(200).json({
             data : rows,
             moreData : fieldData
